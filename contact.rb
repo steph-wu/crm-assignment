41cbe9885494e50d13b1d786dfacd5a4d1e998a1
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-20 13:55:23 -0500

    Create find by id method

--- contact.rb
+++ contact.rb
@@ -1,28 +1,47 @@
 class Contact
+  attr_accessor :first_name, :last_name, :email, :note
+  attr_reader :id
+
+  @@contacts = []
+  @@next_id = 1000
 
   # This method should initialize the contact's attributes
-  def initialize
-
+  def initialize(first_name, last_name, email, note = "n/a")
+    @id = @@next_id
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
   end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, note)
+    # 1. Initialize a new Contact with a unique ID
+    new_contact = self.new(first_name, last_name, email, note)
+    # 2. Add the new Contact to the list of all contacts
+    @@contacts << new_contact
+    # 3. Increment the next unique ID
+    @@next_id += 1
+    # 4. Return new object
+    new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
-
+    @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
+  def self.find(id)
+    @@contacts.each do |contact|
+      return contact if contact.next_id == id
+    end
 
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -44,7 +63,7 @@ class Contact
   end
 
   def full_name
-
+    "#{firstname} #{last_name}"
   end
 
   # This method should delete the contact
@@ -54,5 +73,14 @@ class Contact
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
 end
+
+##########################
+
+steph = Contact.new("Stephanie", "Wu", "wu.stephanie@live.com")
+lynna = Contact.new("Lynna", "Nguyen", "baekbitch@live.com")
+julie = Contact.new("Julie", "Duong", "mmgoldfish@live.com")
+
+puts steph.first_name
+puts Contact.find(1)
