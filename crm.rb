3d8fe284b9b38ab2f6aaf3d1ed1facfdaf89c4ad
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-23 10:13:59 -0500

    Create menu and call option methods

--- crm.rb
+++ crm.rb
@@ -5,15 +5,32 @@ class CRM
   end
 
   def main_menu
-
+    while true # repeat indefinitely
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
   end
 
-  def call_option
-
+  def call_option(user_selected)
+    case user_selected
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+    when 3 then delete_content
+    when 4 then display_all_contacts
+    when 5 then search_by_attribute
+    when 6 then exit
+    end
   end
 
   def add_new_contact
@@ -29,11 +46,11 @@ class CRM
   end
 
   def display_all_contacts
-    
+
   end
 
   def search_by_attribute
-    
+
   end
 
 
